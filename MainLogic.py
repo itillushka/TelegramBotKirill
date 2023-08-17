3b6dd41d4207fd6b691a48e92b32018fc44e6022
illia_pastushok <276487@student.pwr.edu.pl>
2023-08-17 12:37:50 +0200

    changing to telebot API

--- MainLogic.py
+++ MainLogic.py
@@ -1,14 +1,14 @@
 import os
 import pandas as pd
-from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext
+import telebot
+from telebot import types
 
 TOKEN = "6633230318:AAEPmoWn2SgZsenyflbzZEP2hJ_Fgg6-diM"
 DATA_FILE = "Data/user_data.xlsx"
 BROKER_PASSWORD = "1234"
 AUTHORIZED_BROKERS = set()
 
-SELECT_ROLE, DRIVER_DETAILS, BROKER_CARGO, ADD_CARGO, VERIFY_PASSWORD = range(5)
+bot = telebot.TeleBot(TOKEN)
 
 DRIVER_QUESTIONS = [
     "Ваше полное имя?",
@@ -18,47 +18,56 @@ DRIVER_QUESTIONS = [
     "На какую дистанцию готовы ездить? (в километрах)"
 ]
 
-def start(update: Update, context: CallbackContext) -> int:
-    keyboard = [['Брокер', 'Водитель']]
-    update.message.reply_text("Привет! Выберите вашу роль:", reply_markup=ReplyKeyboardMarkup(keyboard, one_time_keyboard=True))
-    return SELECT_ROLE
 
-def select_role(update: Update, context: CallbackContext) -> int:
-    user_id = update.effective_user.id
-    role = update.message.text.lower()
-    context.user_data["role"] = role
+@bot.message_handler(commands=['start'])
+def start(message):
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    buttons = [types.KeyboardButton(text="Брокер"), types.KeyboardButton(text="Водитель")]
+    keyboard.add(*buttons)
+    bot.send_message(message.chat.id, "Привет! Выберите вашу роль:", reply_markup=keyboard)
+    bot.register_next_step_handler(message, select_role)
+
+
+def select_role(message):
+    user_id = message.from_user.id
+    role = message.text.lower()
 
     if role == "брокер":
-        update.message.reply_text("Введите пароль для доступа к роли 'брокер':")
-        return ADD_CARGO
+        bot.send_message(user_id, "Введите пароль для доступа к роли 'брокер':")
+        bot.register_next_step_handler(message, verify_password)
     elif role == "водитель":
-        context.user_data["driver_question_index"] = 0
-        update.message.reply_text(DRIVER_QUESTIONS[0])
-        return DRIVER_DETAILS
+        bot.send_message(user_id, DRIVER_QUESTIONS[0])
+        bot.register_next_step_handler(message, driver_details)
     else:
-        update.message.reply_text("Пожалуйста, выберите 'Брокер' или 'Водитель'.")
+        keyboard = types.ReplyKeyboardRemove()
+        bot.send_message(user_id, "Пожалуйста, выберите 'Брокер' или 'Водитель'.", reply_markup=keyboard)
 
 
-def driver_details(update: Update, context: CallbackContext) -> int:
-    user_id = update.effective_user.id
-    current_question_index = context.user_data["driver_question_index"]
+def driver_details(message):
+    user_id = message.from_user.id
+    current_question_index = message.chat.id
 
-    context.user_data[f"question_{current_question_index + 1}"] = update.message.text
+    if not hasattr(bot, 'user_data'):
+        bot.user_data = {}
+    if "driver_question_index" not in bot.user_data:
+        bot.user_data["driver_question_index"] = 0
+    if "question_data" not in bot.user_data:
+        bot.user_data["question_data"] = {}
+
+    bot.user_data["question_data"][current_question_index + 1] = message.text
 
     if current_question_index + 1 < len(DRIVER_QUESTIONS):
         next_question = DRIVER_QUESTIONS[current_question_index + 1]
-        context.user_data["driver_question_index"] = current_question_index + 1
-        update.message.reply_text(next_question)
+        bot.user_data["driver_question_index"] = current_question_index + 1
+        bot.send_message(user_id, next_question)
+        bot.register_next_step_handler(message, driver_details)
     else:
-        save_driver_info(user_id, context.user_data)
-        update.message.reply_text("Информация сохранена. Спасибо!", reply_markup=ReplyKeyboardRemove())
-        return ConversationHandler.END
+        save_driver_info(user_id, bot.user_data)
+        bot.send_message(user_id, "Информация сохранена. Спасибо!")
+        bot.user_data = {}
 
 
-def save_broker_info(update: Update, context: CallbackContext) -> int:
-    user_id = update.effective_user.id
-    cargo = update.message.text
-
+def save_broker_info(user_id, cargo):
     if not os.path.exists(DATA_FILE):
         df = pd.DataFrame(columns=["User ID", "Role", "Cargo", "Car"])
     else:
@@ -68,10 +77,10 @@ def save_broker_info(update: Update, context: CallbackContext) -> int:
     df = pd.concat([df, new_row], ignore_index=True)
     df.to_excel(DATA_FILE, index=False)
 
-    update.message.reply_text("Информация сохранена. Спасибо!", reply_markup=ReplyKeyboardRemove())
-    return ConversationHandler.END
+    bot.send_message(user_id, "Информация сохранена. Спасибо!")
 
-def save_driver_info(user_id, user_data) -> None:
+
+def save_driver_info(user_id, user_data):
     if not os.path.exists(DATA_FILE):
         df = pd.DataFrame(columns=["User ID"] + [f"Question {i + 1}" for i in range(len(DRIVER_QUESTIONS))])
     else:
@@ -79,61 +88,43 @@ def save_driver_info(user_id, user_data) -> None:
 
     new_row = {"User ID": user_id}
     for i, question in enumerate(DRIVER_QUESTIONS):
-        new_row[f"Question {i + 1}"] = user_data.get(f"question_{i + 1}", "")
+        new_row[f"Question {i + 1}"] = user_data["question_data"].get(i + 1, "")
 
     new_row_df = pd.DataFrame([new_row])
     df = pd.concat([df, new_row_df], ignore_index=True)
     df.to_excel(DATA_FILE, index=False)
 
-def add_cargo(update: Update, context: CallbackContext) -> int:
-    user_id = update.effective_user.id
-    cargo = update.message.text
-    role = context.user_data.get("role")
+
+def verify_password(message):
+    user_id = message.from_user.id
+    password = message.text.strip()
+    if password == BROKER_PASSWORD:
+        bot.user_data["authorized_broker"] = True
+        bot.send_message(user_id, "Пароль верен. Теперь введите ваш груз:")
+        bot.register_next_step_handler(message, add_cargo)
+    else:
+        keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+        buttons = [types.KeyboardButton(text="Брокер"), types.KeyboardButton(text="Водитель")]
+        keyboard.add(*buttons)
+        bot.send_message(user_id, "Неверный пароль. Выберите 'Брокер' или 'Водитель'.", reply_markup=keyboard)
+
+
+def add_cargo(message):
+    user_id = message.from_user.id
+    cargo = message.text
+    role = bot.user_data.get("role")
 
     if role == "брокер":
-        if context.user_data.get("authorized_broker"):
-            save_broker_info(update, context)
-            return ConversationHandler.END
+        if bot.user_data.get("authorized_broker"):
+            save_broker_info(user_id, cargo)
+            bot.user_data = {}
         else:
-            update.message.reply_text("Введите пароль для доступа к роли 'брокер':")
-            return VERIFY_PASSWORD
+            bot.send_message(user_id, "Введите пароль для доступа к роли 'брокер':")
+            bot.register_next_step_handler(message, verify_password)
     else:
-        update.message.reply_text("Пожалуйста, выберите 'Брокер' или 'Водитель'.")
+        keyboard = types.ReplyKeyboardRemove()
+        bot.send_message(user_id, "Пожалуйста, выберите 'Брокер' или 'Водитель'.", reply_markup=keyboard)
 
-def verify_password(update: Update, context: CallbackContext) -> int:
-    password = update.message.text.strip()
-    if password == BROKER_PASSWORD:
-        context.user_data["authorized_broker"] = True
-        update.message.reply_text("Пароль верен. Теперь введите ваш груз:")
-        return ADD_CARGO
-    else:
-        update.message.reply_text("Неверный пароль. Выберите 'Брокер' или 'Водитель'.")
-        return SELECT_ROLE
-def cancel(update: Update, context: CallbackContext) -> int:
-    update.message.reply_text("Операция отменена.", reply_markup=ReplyKeyboardRemove())
-    return ConversationHandler.END
 
-def main() -> None:
-    updater = Updater(TOKEN)
-    dp = updater.dispatcher
-
-    conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
-        states={
-            SELECT_ROLE: [MessageHandler(Filters.text & ~Filters.command, select_role)],
-            VERIFY_PASSWORD: [MessageHandler(Filters.text & ~Filters.command, verify_password)],
-            BROKER_CARGO: [MessageHandler(Filters.text & ~Filters.command, add_cargo)],
-            DRIVER_DETAILS: [MessageHandler(Filters.text & ~Filters.command, driver_details)],
-            ADD_CARGO: [MessageHandler(Filters.text & ~Filters.command, add_cargo)],
-        },
-        fallbacks=[MessageHandler(Filters.command, cancel)],
-    )
-
-    dp.add_handler(conv_handler)
-
-    updater.start_polling()
-    updater.idle()
-
-if __name__ == '__main__':
-    main()
-
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
