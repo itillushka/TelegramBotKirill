0026f238736e7b0915bb65ac3cbae8b2beb11a8a
illia_pastushok <276487@student.pwr.edu.pl>
2023-08-19 12:20:00 +0200

    Changed ReplyButton to InlineButton, added check algorithm in order to verity if user was registered

--- MainLogic.py
+++ MainLogic.py
@@ -5,37 +5,54 @@ from telebot import types
 TOKEN = '6633230318:AAEPmoWn2SgZsenyflbzZEP2hJ_Fgg6-diM'
 broker_password = "1234"
 bot = telebot.TeleBot(TOKEN)
+DATA = "Data/user_data.xlsx"
 
 # Словарь для хранения данных о пользователях
 user_data = {}
 waiting_for_password = {}
 
+
+def is_user_registered(user_id):
+    workbook = openpyxl.load_workbook(DATA)
+    sheet = workbook.active
+    for row in sheet.iter_rows(min_row=1, values_only=True):
+        print(row)
+        if row[0] == user_id:
+            workbook.close()
+            return True
+    workbook.close()
+    return False
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def start(message):
-    markup = types.ReplyKeyboardMarkup(row_width=2)
-    broker_button = types.KeyboardButton("Брокер")
-    driver_button = types.KeyboardButton("Водитель")
-    markup.add(broker_button, driver_button)
-    bot.send_message(message.chat.id, "Выберите вашу роль:", reply_markup=markup)
+    markup = types.InlineKeyboardMarkup(row_width=1)
+    broker_button = types.InlineKeyboardButton("[🚚 Перевозчикам]",callback_data="driver")
+    driver_button = types.InlineKeyboardButton("[ 📞 Диспетчерам]",callback_data="broker")
+    cargo_button = types.InlineKeyboardButton("[ 📦 Отправить груз]",callback_data="cargo")
+    markup.add(broker_button, driver_button, cargo_button)
+    bot.send_message(message.chat.id, "Приветствую в нашем боте! Пожалуйста выберите раздел:", reply_markup=markup)
 
 
-# Обработчик выбора роли с кнопками
-@bot.message_handler(func=lambda message: message.text == "Брокер" or message.text == "Водитель")
-def handle_role_choice(message):
-    user_id = message.from_user.id
-    role = message.text
+@bot.callback_query_handler(func=lambda call: call.data in ["broker", "driver"])
+def handle_role_choice(call):
+    user_id = call.from_user.id
+    role = call.data
 
-    if role == "Брокер":
+    if is_user_registered(user_id):
+        bot.send_message(user_id, "У вас уже есть роль.")
+        return
+
+    if role == "broker":
         user_data[user_id] = {"role": "Брокер"}
         waiting_for_password[user_id] = True
         bot.send_message(user_id, "Введите пароль для доступа к роли брокера:")
-    elif role == "Водитель":
+    elif role == "driver":
         user_data[user_id] = {"role": "Водитель"}
         bot.send_message(user_id, "Отлично! Пожалуйста, ответьте на несколько вопросов.")
         bot.send_message(user_id, "Ваше полное имя?")
-        bot.register_next_step_handler(message, ask_age)
+        bot.register_next_step_handler(call.message, ask_age)
 
+# Обработчик ввода пароля
 @bot.message_handler(func=lambda message: waiting_for_password.get(message.from_user.id))
 def check_broker_password(message):
     user_id = message.from_user.id
@@ -44,8 +61,11 @@ def check_broker_password(message):
     if password == broker_password:
         add_to_excel(user_id, **user_data[user_id])
         bot.send_message(user_id, "Пароль верный. Теперь вы брокер.")
+        waiting_for_password[user_id] = False
+
     else:
         bot.send_message(user_id, "Пароль неверный. Попробуйте еще раз или выберите другую роль.")
+
 def ask_age(message):
     user_id = message.from_user.id
     user_data[user_id]["name"] = message.text
@@ -89,12 +109,12 @@ def save_driver_info(message):
 
 
 def add_to_excel(user_id, **data):
-    workbook = openpyxl.load_workbook('Data/user_data.xlsx')
+    workbook = openpyxl.load_workbook(DATA)
     sheet = workbook.active
 
     sheet.append([user_id, data.get("role"), data.get("name"), data.get("age"), data.get("car"), data.get("city"),
                   data.get("distance")])
-    workbook.save('Data/user_data.xlsx')
+    workbook.save(DATA)
     workbook.close()
 
 
