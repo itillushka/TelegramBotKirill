d93b2ebcc446a944d89d498bcd093a5deca50663
illia_pastushok <276487@student.pwr.edu.pl>
2023-09-08 11:41:06 +0200

    Host 1.x images issue

--- Logic/user_dict.py
+++ Logic/user_dict.py
@@ -5,11 +5,11 @@ driver_data = {}
 
 
 # Пути ко всем изображениям
-CARGO_PHOTO = "telegrambotkirill/resources/Frame 32.png"
-CARGO_LIST_PHOTO = "telegrambotkirill/resources/Frame 33.png"
-START_PHOTO = "telegrambotkirill/resources/Frame 34.png"
-REGISTRATION_PHOTO = "telegrambotkirill/resources/Frame 35.png"
-USER_DATA_PHOTO = "telegrambotkirill/resources/Frame 36.png"
-BROKER_PHOTO = "telegrambotkirill/resources/Frame 37.png"
-NEW_CARGO_PHOTO = "telegrambotkirill/resources/Frame 39.png"
-CARGO_HISTORY_PHOTO = "telegrambotkirill/resources/Frame 40.png"
+CARGO_PHOTO = "resources/Frame 32.png"
+CARGO_LIST_PHOTO = "resources/Frame 33.png"
+START_PHOTO = "resources/Frame 34.png"
+REGISTRATION_PHOTO = "resources/Frame 35.png"
+USER_DATA_PHOTO = "resources/Frame 36.png"
+BROKER_PHOTO = "resources/Frame 37.png"
+NEW_CARGO_PHOTO = "resources/Frame 39.png"
+CARGO_HISTORY_PHOTO = "resources/Frame 40.png"
