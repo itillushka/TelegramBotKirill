e77c72045c5dee5f151dd784a7cbf2ae50bdf3e7
illia_pastushok <276487@student.pwr.edu.pl>
2023-09-08 11:37:25 +0200

    Host 1.8 image path issue

--- Logic/user_dict.py
+++ Logic/user_dict.py
@@ -5,11 +5,11 @@ driver_data = {}
 
 
 # Пути ко всем изображениям
-CARGO_PHOTO = "C:/Users/illia/GitProjects/telegrambotkirill/resources/Frame 32.png"
-CARGO_LIST_PHOTO = "C:/Users/illia/GitProjects/telegrambotkirill/resources/Frame 33.png"
-START_PHOTO = "C:/Users/illia/GitProjects/telegrambotkirill/resources/Frame 34.png"
-REGISTRATION_PHOTO = "C:/Users/illia/GitProjects/telegrambotkirill/resources/Frame 35.png"
-USER_DATA_PHOTO = "C:/Users/illia/GitProjects/telegrambotkirill/resources/Frame 36.png"
-BROKER_PHOTO = "C:/Users/illia/GitProjects/telegrambotkirill/resources/Frame 37.png"
-NEW_CARGO_PHOTO = "C:/Users/illia/GitProjects/telegrambotkirill/resources/Frame 39.png"
-CARGO_HISTORY_PHOTO = "C:/Users/illia/GitProjects/telegrambotkirill/resources/Frame 40.png"
+CARGO_PHOTO = "telegrambotkirill/resources/Frame 32.png"
+CARGO_LIST_PHOTO = "telegrambotkirill/resources/Frame 33.png"
+START_PHOTO = "telegrambotkirill/resources/Frame 34.png"
+REGISTRATION_PHOTO = "telegrambotkirill/resources/Frame 35.png"
+USER_DATA_PHOTO = "telegrambotkirill/resources/Frame 36.png"
+BROKER_PHOTO = "telegrambotkirill/resources/Frame 37.png"
+NEW_CARGO_PHOTO = "telegrambotkirill/resources/Frame 39.png"
+CARGO_HISTORY_PHOTO = "telegrambotkirill/resources/Frame 40.png"
